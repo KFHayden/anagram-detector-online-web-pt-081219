f0e359428321f1066dfd398a94341e496cf1f985
Kevin Hayden <kevin.fhayden@gmail.com>
2019-09-18 22:37:54 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,8 +9,7 @@ class Anagram
   def match(word)
     #uses the select method to return an array of all anagrams
     #if no matches, returns an empty array
-    array = @word.split(" ")
-    array(word)
+    @word(word).split(" ")
   end  
  
   
