c2a73947abde04720a5e9d3f4ecfd21dedb0b6ab
Kevin Hayden <kevin.fhayden@gmail.com>
2019-09-18 22:27:05 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @words = word
+  end
+  
+  def match(word)
+    #uses the select method to return an array of all anagrams
+    #if no matches, returns an empty array
+    @words = %w(@words)
+    
+  end
+  
+end
