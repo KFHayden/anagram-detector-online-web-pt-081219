6f1db69bdc5dc4ed6a7e3902004a679c633aba98
Kevin Hayden <kevin.fhayden@gmail.com>
2019-09-18 22:33:09 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,8 +9,8 @@ class Anagram
   def match(word)
     #uses the select method to return an array of all anagrams
     #if no matches, returns an empty array
-    
-    array = @word.sort
+    array = @word.split(" ")
+    sorted_array = array.sort
     sorted_array.select do |words|
       if sorted_array == words
         words
